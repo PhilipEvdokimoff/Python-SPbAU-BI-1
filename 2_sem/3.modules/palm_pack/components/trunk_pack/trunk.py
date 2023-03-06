1e320d24d0a2c5e4d4c10ce5067f03ab52447167
piliff-e <philipp.evdokimov@icloud.com>
2023-03-07 00:20:19 +0300

    Task 3: modules

---
+++ 2_sem/3.modules/palm_pack/components/trunk_pack/trunk.py
@@ -0,0 +1,21 @@
+import turtle as tl
+import trunk_left_right  # Подключение модуля на одном уровне
+
+
+def trunk(size):
+    """
+    Построение всего ствола
+    """
+    x0, y0 = tl.xcor(), tl.ycor()
+    tl.color('black', 'brown')
+    tl.begin_fill()
+    trunk_left_right.trunk_left(size, 0)  # Использована функция из модуля
+    x1, y1 = tl.xcor(), tl.ycor()
+    tl.penup()
+    tl.goto(x0, y0)
+    tl.pendown()
+    tl.goto(x0 + 50, y0)
+    tl.setheading(0)
+    trunk_left_right.trunk_right(size, 0)  # Использована функция из модуля
+    tl.goto(x1, y1)
+    tl.end_fill()
---
+++ 2_sem/3.modules/result.py
@@ -0,0 +1,3 @@
+from palm_pack import draw_palm  # Подключение модуля на уровень глубже
+
+draw_palm.draw_palm(80, 162, 5)  # Использована функция из модуля
